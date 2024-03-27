47c429cbaba9ad763d3fcc3aff06180b336a461c
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-03-27 16:23:33 +0100

    create page structure index, show, create

---
+++ resources/views/characters/show.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Pagina iniziale')
+
+@section('main-content')
+    <section>
+        <div class="container py-4">
+            
+        </div>
+    </section>
+@endsection
