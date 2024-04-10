24648408d0410a3d63bb296fdc832e2bae2c4b83
Paky-Simeone <pasquale.simeone.46@gmail.com>
2024-04-10 16:03:12 +0200

    add Type Table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             ItemSeeder::class,
-            CharacterSeeder::class
+            TypeSeeder::class,
+            CharacterSeeder::class,
         ]);
     }
 }
